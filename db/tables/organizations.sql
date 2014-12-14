41b734486b04b288546c9e31c2e6b1671075d2cb
Brian Hiatt <bchiatt>
2014-12-14 15:13:13 -0600

    add org table

---
+++ db/tables/organizations.sql
@@ -0,0 +1,8 @@
+create table orgs(
+  id serial primary key,
+  id_admin integer references users(id),
+  name varchar(255) unique,
+  city varchar(255),
+  state varchar(2),
+  created_at timestamp not null default now()
+);
