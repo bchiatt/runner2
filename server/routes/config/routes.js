65122eef3a18570c03ebdb7c0723db555ab2642d
Brian Hiatt <bchiatt>
2014-12-16 12:45:41 -0600

    rename register route

--- server/routes/config/routes.js
+++ server/routes/config/routes.js
@@ -2,7 +2,7 @@
 
 module.exports = [
   {method: 'get',    path: '/{param*}', config: require('../definitions/general/static')},
-  {method: 'post',   path: '/register', config: require('../definitions/users/register_user')},
+  {method: 'post',   path: '/register', config: require('../definitions/users/register')},
   {method: 'post',   path: '/login',    config: require('../definitions/users/login')},
   {method: 'delete', path: '/logout',   config: require('../definitions/users/logout')},
   {method: 'get',    path: '/status',   config: require('../definitions/users/status')}
