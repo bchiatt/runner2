36ed430f8a4a856b21017eb37426e0e57dff8dfd
Brian Hiatt <bchiatt>
2014-12-16 17:25:49 -0600

    add register and login acceptance tests

--- server/routes/definitions/users/register.js
+++ server/routes/definitions/users/register.js
@@ -25,11 +25,15 @@ module.exports = {
         reply(results).code(!results && !err ? 200 : 400);
       });
     }else{
-      Org.register(request.payload, function(err, org){
-        if(err){return reply().code(400);}
-        User.register(request.payload, org.id, function(err, user){
-          Org.changeAdmin(org.id, user.id, function(err, results){
-            reply().code(results && !err ? 200 : 400);
+      Org.findByName(request.payload, function(err, results){
+        if(results || err){return reply().code(400);}
+        Org.register(request.payload, function(err, org){
+          //if(err){return reply().code(400);}
+          User.register(request.payload, org.id, function(err, user){
+            if(err || !user){return reply().code(400);}
+            Org.changeAdmin(org.id, user.id, function(err, results){
+              reply().code(results && !err ? 200 : 400);
+            });
           });
         });
       });
--- test/acceptance/users.js
+++ test/acceptance/users.js
@@ -1,39 +1,148 @@
-/*
+/* jshint expr:true */
+
 'use strict';
 
-process.env.DB   = 'mongodb://localhost/runner2-test';
+var expect     = require('chai').expect,
+cp         = require('child_process'),
+h          = require('../helpers/helpers'),
+server     = require('../../server/index'),
+Lab        = require('lab'),
+lab        = exports.lab = Lab.script(),
+describe   = lab.describe,
+it         = lab.it,
+beforeEach = lab.beforeEach,
+db         = h.getdb();
 
-var expect  = require('chai').expect,
-    cp      = require('child_process'),
-    app     = require('../../server/index'),
-    cookie  = null,
-    request = require('supertest');
-
-describe('users', function(){
-  before(function(done){
-    request(app).get('/').end(done);
-  });
+describe('Users', function(){
+  var cookie;
 
   beforeEach(function(done){
-    cp.execFile(__dirname + '/../scripts/clean-db.sh', ['runner2-test'], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
-      request(app)
-      .post('/login')
-      .send('email=bob@aol.com')
-      .send('password=1234')
-      .end(function(err, res){
-        cookie = res.headers['set-cookie'][0];
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      var options = {
+        method: 'post',
+        url: '/login',
+        payload: {
+          username: 'bob',
+          password: '1234'
+        }
+      };
+
+      server.inject(options, function(response){
+        cookie = response.headers['set-cookie'][0].match(/hapi-cookie=[^;]+/)[0];
         done();
       });
     });
   });
 
   describe('post /register', function(){
-    it('should register a user', function(done){
-      request(app)
-      .post('/register')
-      .send({email:'sally@aol.com', password:'ninja'})
-      .end(function(err, res){
-        expect(res.status).to.equal(200);
+    it('should register a new user and org', function(done){
+      var options = {
+        method: 'post',
+        url: '/register',
+        payload: {
+          orgName: 'NSS',
+          city: 'Nashville',
+          state: 'TN',
+          first: 'Sam',
+          last: 'Iam',
+          username: 'sam',
+          password: '456'
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it('should confirm org is registered', function(done){
+      var options = {
+        method: 'post',
+        url: '/register',
+        payload: {
+          orgName: 'trevecca',
+          city: 'Nashville',
+          state: 'TN'
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(400);
+        done();
+      });
+    });
+    it('should confirm org is NOT registered', function(done){
+      var options = {
+        method: 'post',
+        url: '/register',
+        payload: {
+          orgName: 'NSS',
+          city: 'Nashville',
+          state: 'TN'
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it('should NOT register a new user and org - user exists', function(done){
+      var options = {
+        method: 'post',
+        url: '/register',
+        payload: {
+          orgName: 'NSS',
+          city: 'Nashville',
+          state: 'TN',
+          first: 'Bob',
+          last: 'Tom',
+          username: 'bob',
+          password: '1234'
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(400);
+        done();
+      });
+    });
+    it('should NOT register a new user and org - org exists', function(done){
+      var options = {
+        method: 'post',
+        url: '/register',
+        payload: {
+          orgName: 'Trevecca',
+          city: 'Nashville',
+          state: 'TN',
+          first: 'Bob',
+          last: 'Tom',
+          username: 'bob',
+          password: '1234'
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(400);
+        done();
+      });
+    });
+  });
+
+  describe('post /login', function(){
+    it('should login a user', function(done){
+      var options = {
+        method: 'post',
+        url: '/login',
+        payload: {
+          username: 'bob',
+          password: '1234'
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        expect(response.result.username).to.equal('bob');
         done();
       });
     });
@@ -41,14 +150,35 @@ describe('users', function(){
 
   describe('delete /logout', function(){
     it('should logout a user', function(done){
-      request(app)
-      .delete('/logout')
-      .set('cookie', cookie)
-      .end(function(err, res){
-        expect(res.status).to.equal(200);
+      var options = {
+        method: 'delete',
+        url: '/logout',
+        headers:{
+          cookie: cookie
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  describe('get /status', function(){
+    it('should get status for a user', function(done){
+      var options = {
+        method: 'get',
+        url: '/status',
+        headers:{
+          cookie: cookie
+        }
+      };
+
+      server.inject(options, function(response){
+        expect(response.statusCode).to.equal(200);
         done();
       });
     });
   });
 });
-*/
