092a9f7ddfe8169a34a06c90400ea61b6919bf97
Brian Hiatt <bchiatt>
2015-01-10 21:26:40 -0600

    add, update, delete treatments

--- client/views/treatments/treatments.js
+++ client/views/treatments/treatments.js
@@ -2,9 +2,86 @@
   'use strict';
 
   angular.module('runner2')
-  .controller('TreatmentsCtrl', ['$scope', 'Treatment', function($scope, Treatment){
-    Treatment.all().then(function(response){
-      console.log(response.data.treatments);
+  .controller('TreatmentsCtrl', ['$scope', 'Treatment', 'Time', 'Therapist', 'Discipline', 'Client', 'Insurance', function($scope, Treatment, Time, Therapist, Discipline, Client, Insurance){
+    var  clients, disciplines, insurances, therapists;
+    $scope.modalShown = false;
+    $scope.treatments = [];
+    $scope.selected = {};
+
+    getAll();
+
+    function getAll(){
+      Treatment.all().then(function(response){
+        $scope.treatments = response.data.treatments || [];
+      });
+    }
+
+    Client.all().then(function(response){
+      clients = response.data.clients;
     });
+
+    Therapist.all().then(function(response){
+      therapists = response.data.therapists;
+    });
+
+    Discipline.all().then(function(response){
+      disciplines = response.data.disciplines;
+    });
+
+    Insurance.all().then(function(response){
+      insurances = response.data.insurances;
+    });
+
+    $scope.save = function(data){
+      data = cleanData(data);
+      $scope.selected = {};
+
+      if(data.id){
+        Treatment.update(data).then(function(response){
+          getAll();
+        });
+      }else{
+        Treatment.create(data).then(function(response){
+          getAll();
+        }, function(err){console.log('err', err);});
+      }
+    };
+
+    $scope.nuke = function(id){
+      Treatment.nuke(id).then(function(response){
+        getAll();
+      });
+    };
+
+    $scope.dateMoDaYr = function(iso8601){
+      if(!iso8601){return;}
+      return moment(Time.isoDate(iso8601)).format('MMM D, YYYY');
+    };
+
+    $scope.toggleModal = function(treatment){
+      $scope.selected = treatment;
+      $scope.selected.clients = clients;
+      $scope.selected.therapists = therapists;
+      $scope.selected.disciplines = disciplines;
+      $scope.selected.insurances = insurances;
+
+      $scope.modalShown = !$scope.modalShown;
+    };
+
+    function cleanData(data){
+      var obj = {};
+      if(data.id){obj.id = data.id;}
+      obj.client_id = data.client_id;
+      obj.therapist_id = data.therapist_id;
+      obj.disc_id = data.disc_id;
+      obj.ins_id = data.ins_id;
+      obj.mins_expected = data.mins_expected;
+      obj.mins_actual = data.mins_actual;
+      if(data.tx_date){obj.tx_date = Time.postgresDate(data.tx_date);}
+      obj.day_count = data.day_count;
+      obj.is_note_done = data.is_note_done || false;
+      obj.is_archived = data.is_archived || false;
+      return obj;
+    }
   }]);
 })();
--- server/models/treatment.js
+++ server/models/treatment.js
@@ -1,16 +1,20 @@
 'use strict';
 
-var pg     = require('../postgres/manager');
+var pg        = require('../postgres/manager'),
+    multiline = require('multiline');
 
 function Treatment(obj){
-	this.clientId = obj.clientId;
-  this.therId   = obj.therId;
-  this.discId   = obj.discId;
-  this.insId    = obj.insId;
-  this.planId   = obj.planId;
-  this.expMins  = obj.expMins;
-  this.txDate   = obj.txDate;
-  this.dayCount = obj.dayCount;
+	this.clientId = obj.client_id;
+  this.therId   = obj.therapist_id;
+  this.discId   = obj.disc_id;
+  this.insId    = obj.ins_id;
+  this.planId   = obj.plan_id;
+  this.expMins  = obj.mins_expected;
+  this.actMins  = obj.mins_actual;
+  this.txDate   = obj.tx_date;
+  this.dayCount = obj.day_count;
+  this.note     = obj.is_note_done;
+  this.arch     = obj.is_archived;
 }
 
 Treatment.findById = function(user, id, cb){
@@ -21,18 +25,51 @@ Treatment.findById = function(user, id, cb){
 };
 
 Treatment.all = function(user, cb){
-  pg.query('select * from treatments where org_id = $1',
-      [user.org.id], function(err, results){
+	var sql = multiline.stripIndent(function(){/*
+		select
+			tx.id,
+
+			tx.client_id,
+			c.first as client_first,
+			c.last as client_last,
+
+			tx.therapist_id,
+			t.first as therapist_first,
+			t.last as therapist_last,
+
+			tx.disc_id,
+			d.name as disc_name,
+			d.abbr as disc_abbr,
+
+      tx.ins_id,
+			i.name as ins_name,
+
+			tx.mins_expected,
+			tx.mins_actual,
+			tx.tx_date,
+			tx.day_count,
+			tx.is_note_done,
+			tx.is_archived
+		from treatments tx
+		left outer join clients c on c.id = tx.client_id
+		left outer join therapists t on t.id = tx.therapist_id
+		left outer join disciplines d on d.id = tx.disc_id
+		left outer join insurances i on i.id = tx.ins_id
+		where tx.org_id = $1
+		order by tx.tx_date asc
+		*/});
+  pg.query(sql, [user.org.id], function(err, results){
     cb(err, results && results.rows ? results.rows : null);
   });
 };
 
 Treatment.add = function(user, obj, cb){
   var t = new Treatment(obj);
-  pg.query('select treatment_add($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+  pg.query('select treatment_add($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)',
       [
         t.clientId, t.therId, user.org.id, t.discId, t.insId,
-        t.planId, t.expMins, t.txDate, t.dayCount
+        t.planId, t.expMins, t.actMins, t.txDate, t.dayCount,
+				t.note, t.arch
       ],
       function(err, results){
 		cb(err, results && results.rows[0] ? results.rows[0] : null);
@@ -42,8 +79,9 @@ Treatment.add = function(user, obj, cb){
 Treatment.update = function(user, obj, cb){
   pg.query('select treatment_update($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)',
       [
-        obj.id, obj.clientId, obj.therId, user.org.id, obj.discId, obj.insId, obj.planId,
-        obj.expMins, obj.actMins, obj.txDate, obj.dayCount, obj.isNote, obj.isArch
+        obj.id, obj.client_id, obj.therapist_id, user.org.id, obj.disc_id,
+        obj.ins_id, obj.plan_id, obj.mins_expected, obj.mins_actual,
+        obj.tx_date, obj.day_count, obj.is_note_done, obj.is_archived
       ],
       function(err, results){
     cb(err, results && results.rows ? results.rows[0] : null);
